469425e50820983c7ab5e09a400207f68710f13f
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-01-29 19:21:03 +0700

    Update __init__.py
    Signed-off-by: Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>

--- strings/__init__.py
+++ strings/__init__.py
@@ -6,8 +6,10 @@ import yaml
 languages = {}
 languages_present = {}
 
+
 def get_command(value: str) -> List:
     return commands["command"][value]
+
 
 def get_string(lang: str):
     return languages[lang]
@@ -15,20 +17,20 @@ def get_string(lang: str):
 
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
-        languages["id"] = yaml.safe_load(
-            open(r"./strings/langs/id.yml", encoding="utf8")
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
         )
-        languages_present["id"] = languages["id"]["name"]
+        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "id":
+        if language_name == "en":
             continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["id"]:
+        for item in languages["en"]:
             if item not in languages[language_name]:
-                languages[language_name][item] = languages["id"][item]
+                languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name]["name"]
     except:

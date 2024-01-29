9a2d27f97026b436c80532c668179a588babc99b
Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>
2024-01-29 19:22:29 +0700

    Update __init__.py
    Signed-off-by: Rehansaputradewantoro <156561840+Rehansaputradewantoro@users.noreply.github.com>

--- strings/__init__.py
+++ strings/__init__.py
@@ -6,11 +6,6 @@ import yaml
 languages = {}
 languages_present = {}
 
-
-def get_command(value: str) -> List:
-    return commands["command"][value]
-
-
 def get_string(lang: str):
     return languages[lang]
 
